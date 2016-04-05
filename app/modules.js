3babc29bb5a8d5c394704a39778ad12d86220eb0
alexjones <AkJones2007@gmail.com>
2016-04-05 13:17:47 -0400

    Successfully completes module tests.

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,14 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
+    var module = {};
+        module.name = str2;
+        module.greeting = str1;
 
+        module.sayIt = function() {
+          return this.greeting + ', ' + this.name;
+        }
+
+        return module;
   }
 };
