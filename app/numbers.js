1ab482c6ea4106b985f868f79fbff0ef15153ecd
alexjones <AkJones2007@gmail.com>
2016-04-05 14:48:16 -0400

    Successfully completes number tests.

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var binaryString = num.toString(2);
+    var bitAtPosition = binaryString[binaryString.length - bit];
+    return parseInt(bitAtPosition, 10);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    var binaryString = num.toString(2);
+    for (var i = binaryString.length; i < 8; i++) {
+      binaryString = '0' + binaryString;
+    }
+    return binaryString;
   },
 
   multiply: function(a, b) {
-
+    var precision = b.toString().split('.')[1].length;
+    return parseFloat((a * b).toFixed(precision), 10);
   }
 };
