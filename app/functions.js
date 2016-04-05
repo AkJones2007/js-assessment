e1f809d82ac3d6bc188c443ec1e1da35c52459df
alexjones <AkJones2007@gmail.com>
2016-04-05 12:24:08 -0400

    Successfully completes function tests

--- app/functions.js
+++ app/functions.js
@@ -2,38 +2,80 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   functionFunction : function(str) {
-
+    return function(str2) {
+      return str + ', ' + str2;
+    }
   },
 
   makeClosures : function(arr, fn) {
-
+    return arr.map(function(num) {
+      return function() {
+        return fn(num);
+      }
+    });
   },
 
   partial : function(fn, str1, str2) {
-
+    return function(str3) {
+      return fn(str1, str2, str3);
+    }
   },
 
   useArguments : function() {
-
+    var total = 0;
+    for (var num in arguments) {
+      total += arguments[num];
+    }
+    return total;
   },
 
   callIt : function(fn) {
+    var args = [];
 
+    for (var index in arguments) {
+      if (index !== '0') {
+        args.push(arguments[index]);
+      }
+    };
+
+    return fn.apply(this, args);
   },
 
   partialUsingArguments : function(fn) {
+    var args = [];
+    for (var index in arguments) {
+      if (index !== '0') {
+        args.push(arguments[index]);
+      }
+    }
 
+    return function() {
+      for (var index in arguments) {
+        args.push(arguments[index]);
+      }
+      return fn.apply(this, args);
+    }
   },
 
   curryIt : function(fn) {
+    var args = [];
+    var collect = function(arg) {
+      args.push(arg);
 
+      if (args.length < 3) {
+        return collect;
+      } else {
+        return fn.apply(this, args);
+      }
+    };
+    return collect;
   }
 };
