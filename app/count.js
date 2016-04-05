c1c03ebb88a0fc8bf84a8bf282ae7aa4cb9f99c6
alexjones <AkJones2007@gmail.com>
2016-04-05 10:26:14 -0400

    Successfully completes count tests

--- app/count.js
+++ app/count.js
@@ -2,6 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var counter = {
+      current: start,
+      max: end,
 
+      set: function() {
+        console.log(start);
+        this.current += 1;
+        this.print();
+      },
+
+      print: function() {
+        this.timer = window.setTimeout(function() {
+          console.log(this.current);
+          if (this.current < this.max) {
+            this.current += 1;
+            this.print();
+          }
+        }.bind(this), 100);
+      },
+
+      cancel: function() {
+        window.clearTimeout(this.timer);
+      }
+    };
+
+    counter.set();
+    return counter;
   }
 };
