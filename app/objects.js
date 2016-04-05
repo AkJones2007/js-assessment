81267752051e1f754e446ee92d4fd22a18ba4620
alexjones <AkJones2007@gmail.com>
2016-04-05 15:16:52 -0400

    Successfully completes object tests.

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    var ownProps = Object.getOwnPropertyNames(obj);
+    var keyValuePairs = ownProps.map(function(propName) {
+      return propName + ': ' + obj[propName];
+    });
+    return keyValuePairs;
   }
 };
