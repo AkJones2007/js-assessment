a9d15e1996d62025e24f062b42f779cbc29d1f21
alexjones <AkJones2007@gmail.com>
2016-04-05 00:46:19 -0400

    Successfully completes all async tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var promise = new Promise(function(resolve, reject) {
+      if (value) {
+        resolve(value);
+      }
+    });
+    return promise;
   },
 
   manipulateRemoteData : function(url) {
-
+    var promise = new Promise(function(resolve, reject) {
+      $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json'
+      }).done(function(data) {
+        var names = data.people.map(function(person) {
+          return person.name;
+        }).sort();
+        resolve(names);
+      });
+    });
+    return promise;
   }
 };
