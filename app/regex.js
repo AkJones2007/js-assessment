9b1807ae2b249e147d840a2d6bbc1b1207108014
alexjones <AkJones2007@gmail.com>
2016-04-06 17:21:26 -0400

    Successfully completes regex tests

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    var digit = /\d/;
+    return digit.test(str);
   },
 
   containsRepeatingLetter : function(str) {
-
+    var repeats = /([a-zA-Z])\1+/;
+    return repeats.test(str);
   },
 
   endsWithVowel : function(str) {
-
+    var vowels = /[aeiouAEIOU]$/;
+    return vowels.test(str);
   },
 
   captureThreeNumbers : function(str) {
-
+    var threeDigits = /\d\d\d/;
+    var match = str.match(threeDigits);
+    return match ? match[0] : false;
   },
 
   matchesPattern : function(str) {
-
+    var validPhoneNum = /^\d\d\d-\d\d\d-\d\d\d\d$/;
+    return validPhoneNum.test(str);
   },
   isUSD : function(str) {
-
+    var validUSD = /^(\$)((\d{1,3},\d{3})|(\d{1,3}))(,\d{3})*(\.\d{2})?$/;
+    return validUSD.test(str);
   }
 };
